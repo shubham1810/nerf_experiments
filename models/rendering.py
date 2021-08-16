6f20048b3092d448528c2dc8f238edda52b62f55
Shubham Dokania <shubham.k.dokania@gmail.com>
2021-08-16 17:49:23 +0530

    Add rendering method for loss part as well

--- models/rendering.py
+++ models/rendering.py
@@ -393,3 +393,204 @@ def render_2d_rays(models,
         inference(results, models['fine'], 'fine', xyz_fine, z_vals, test_time, **kwargs)
     
     return results
+
+
+def render_loss_rays(models,
+                embeddings,
+                rays,
+                N_samples=64,
+                use_disp=False,
+                perturb=0,
+                noise_std=1,
+                N_importance=0,
+                chunk=1024*32,
+                white_back=False,
+                test_time=False,
+                **kwargs
+                ):
+    """
+    Render rays and Learn a Loss term as well by computing the output of @model applied on @rays
+    Inputs:
+        models: list of NeRF models (coarse and fine) defined in nerf.py
+        embeddings: list of embedding models of origin and direction defined in nerf.py
+        rays: (N_rays, 3+3+2), ray origins and directions, near and far depths
+        N_samples: number of coarse samples per ray
+        use_disp: whether to sample in disparity space (inverse depth)
+        perturb: factor to perturb the sampling position on the ray (for coarse model only)
+        noise_std: factor to perturb the model's prediction of sigma
+        N_importance: number of fine samples per ray
+        chunk: the chunk size in batched inference
+        white_back: whether the background is white (dataset dependent)
+        test_time: whether it is test (inference only) or not. If True, it will not do inference
+                   on coarse rgb to save time
+    Outputs:
+        result: dictionary containing final rgb and depth maps for coarse and fine models
+    """
+
+    def inference(results, model, typ, xyz, z_vals, test_time=False, **kwargs):
+        """
+        Helper function that performs model inference.
+        Inputs:
+            results: a dict storing all results
+            model: NeRF model (coarse or fine)
+            typ: 'coarse' or 'fine'
+            xyz: (N_rays, N_samples_, 3) sampled positions
+                  N_samples_ is the number of sampled points in each ray;
+                             = N_samples for coarse model
+                             = N_samples+N_importance for fine model
+            z_vals: (N_rays, N_samples_) depths of the sampled positions
+            test_time: test time or not
+        Outputs:
+            if weights_only:
+                weights: (N_rays, N_samples_): weights of each sample
+            else:
+                rgb_final: (N_rays, 3) the final rgb image
+                depth_final: (N_rays) depth map
+                weights: (N_rays, N_samples_): weights of each sample
+        """
+        N_samples_ = xyz.shape[1]
+        xyz_ = rearrange(xyz, 'n1 n2 c -> (n1 n2) c') # (N_rays*N_samples_, 3)
+
+        # Perform model inference to get rgb and raw sigma
+        B = xyz_.shape[0]
+        out_chunks = []
+        loss_chunks = []
+        if typ=='coarse' and test_time and 'fine' in models:
+            for i in range(0, B, chunk):
+                xyz_embedded = embedding_xyz(xyz_[i:i+chunk])
+                out_val, out_loss_val = model(xyz_embedded, sigma_only=True)
+                out_chunks += [out_val]
+                loss_chunks += [out_loss_val]
+
+            out = torch.cat(out_chunks, 0)
+            out_loss = torch.cat(loss_chunks, 0)
+
+            sigmas = rearrange(out, '(n1 n2) 1 -> n1 n2', n1=N_rays, n2=N_samples_)
+            loss_sigmas = rearrange(out_loss, '(n1 n2) 1 -> n1 n2', n1=N_rays, n2=N_samples_)
+        else: # infer rgb and sigma and others
+            dir_embedded_ = repeat(dir_embedded, 'n1 c -> (n1 n2) c', n2=N_samples_)
+                            # (N_rays*N_samples_, embed_dir_channels)
+            for i in range(0, B, chunk):
+                xyz_embedded = embedding_xyz(xyz_[i:i+chunk])
+                xyzdir_embedded = torch.cat([xyz_embedded,
+                                             dir_embedded_[i:i+chunk]], 1)
+                out_val, out_loss_val = model(xyzdir_embedded, sigma_only=False)
+                out_chunks += [out_val]
+                loss_chunks += [out_loss_val]
+
+            out = torch.cat(out_chunks, 0)
+            out_loss = torch.cat(loss_chunks, 0)
+
+            # out = out.view(N_rays, N_samples_, 4)
+            out = rearrange(out, '(n1 n2) c -> n1 n2 c', n1=N_rays, n2=N_samples_, c=4)
+            out_loss = rearrange(out_loss, '(n1 n2) c -> n1 n2 c', n1=N_rays, n2=N_samples_, c=2)
+
+            rgbs = out[..., :3] # (N_rays, N_samples_, 3)
+            sigmas = out[..., 3] # (N_rays, N_samples_)
+
+            sigma_loss = out_loss[..., 0]
+            rgb_loss = out_loss[..., 1]
+            
+        # Convert these values using volume rendering (Section 4)
+        deltas = z_vals[:, 1:] - z_vals[:, :-1] # (N_rays, N_samples_-1)
+        delta_inf = 1e10 * torch.ones_like(deltas[:, :1]) # (N_rays, 1) the last delta is infinity
+        deltas = torch.cat([deltas, delta_inf], -1)  # (N_rays, N_samples_)
+
+        # compute alpha by the formula (3)
+        noise = torch.randn_like(sigmas) * noise_std
+        alphas = 1-torch.exp(-deltas*torch.relu(sigmas+noise)) # (N_rays, N_samples_)
+        # For loss module
+        alpha_loss = 1-torch.exp(-deltas*torch.relu(sigma_loss+noise))
+
+        alphas_shifted = \
+            torch.cat([torch.ones_like(alphas[:, :1]), 1-alphas+1e-10], -1) # [1, 1-a1, 1-a2, ...]
+        alpha_loss_shifted = \
+            torch.cat([torch.ones_like(alpha_loss[:, :1]), 1-alpha_loss+1e-10], -1)
+
+        weights = \
+            alphas * torch.cumprod(alphas_shifted[:, :-1], -1) # (N_rays, N_samples_)
+        weight_loss = \
+            alpha_loss * torch.cumprod(alpha_loss_shifted[:, :-1], -1)
+        
+        weights_sum = reduce(weights, 'n1 n2 -> n1', 'sum') # (N_rays), the accumulated opacity along the rays
+                                                            # equals "1 - (1-a1)(1-a2)...(1-an)" mathematically
+        weight_loss_sum = reduce(weight_loss, 'n1 n2 -> n1', 'sum')
+
+        # print(reduce(wts, 'n1 n2 -> n1', 'sum'), weights_sum)
+
+        results[f'weights_{typ}'] = weights
+        results[f'opacity_{typ}'] = weights_sum
+        results[f'z_vals_{typ}'] = z_vals
+
+        results[f'weight_loss_{typ}'] = weight_loss
+        results[f'opacity_loss_{typ}'] = weight_loss_sum
+
+        if test_time and typ == 'coarse' and 'fine' in models:
+            return
+
+        rgb_map = reduce(rearrange(weights, 'n1 n2 -> n1 n2 1')*rgbs, 'n1 n2 c -> n1 c', 'sum')
+        rgb_loss_map = reduce(weight_loss*rgb_loss, 'n1 n2 -> n1', 'sum')
+
+        depth_map = reduce(weights*z_vals, 'n1 n2 -> n1', 'sum')
+        depth_loss_map = reduce(weight_loss*z_vals, 'n1 n2 -> n1', 'sum')
+
+        if white_back:
+            rgb_map += 1-weights_sum.unsqueeze(1)
+
+        results[f'rgb_{typ}'] = rgb_map
+        results[f'depth_{typ}'] = depth_map
+
+        results[f'rgb_loss_{typ}'] = rgb_loss_map
+        results[f'depth_loss_{typ}'] = depth_loss_map
+
+        return
+
+    embedding_xyz, embedding_dir = embeddings['xyz'], embeddings['dir']
+
+    # Decompose the inputs
+    N_rays = rays.shape[0]
+    rays_o, rays_d = rays[:, 0:3], rays[:, 3:6] # both (N_rays, 3)
+    near, far = rays[:, 6:7], rays[:, 7:8] # both (N_rays, 1)
+    # Embed direction
+    dir_embedded = embedding_dir(kwargs.get('view_dir', rays_d)) # (N_rays, embed_dir_channels)
+
+    rays_o = rearrange(rays_o, 'n1 c -> n1 1 c')
+    rays_d = rearrange(rays_d, 'n1 c -> n1 1 c')
+
+    # Sample depth points
+    z_steps = torch.linspace(0, 1, N_samples, device=rays.device) # (N_samples)
+    if not use_disp: # use linear sampling in depth space
+        z_vals = near * (1-z_steps) + far * z_steps
+    else: # use linear sampling in disparity space
+        z_vals = 1/(1/near * (1-z_steps) + 1/far * z_steps)
+
+    z_vals = z_vals.expand(N_rays, N_samples)
+    
+    if perturb > 0: # perturb sampling depths (z_vals)
+        z_vals_mid = 0.5 * (z_vals[: ,:-1] + z_vals[: ,1:]) # (N_rays, N_samples-1) interval mid points
+        # get intervals between samples
+        upper = torch.cat([z_vals_mid, z_vals[: ,-1:]], -1)
+        lower = torch.cat([z_vals[: ,:1], z_vals_mid], -1)
+        
+        perturb_rand = perturb * torch.rand_like(z_vals)
+        z_vals = lower + (upper - lower) * perturb_rand
+
+    xyz_coarse = rays_o + rays_d * rearrange(z_vals, 'n1 n2 -> n1 n2 1')
+
+    results = {}
+    inference(results, models['coarse'], 'coarse', xyz_coarse, z_vals, test_time, **kwargs)
+
+    if N_importance > 0: # sample points for fine model
+        z_vals_mid = 0.5 * (z_vals[: ,:-1] + z_vals[: ,1:]) # (N_rays, N_samples-1) interval mid points
+        z_vals_ = sample_pdf(z_vals_mid, results['weights_coarse'][:, 1:-1].detach(),
+                             N_importance, det=(perturb==0))
+                  # detach so that grad doesn't propogate to weights_coarse from here
+
+        z_vals = torch.sort(torch.cat([z_vals, z_vals_], -1), -1)[0]
+                 # combine coarse and fine samples
+
+        xyz_fine = rays_o + rays_d * rearrange(z_vals, 'n1 n2 -> n1 n2 1')
+
+        inference(results, models['fine'], 'fine', xyz_fine, z_vals, test_time, **kwargs)
+
+    return results
